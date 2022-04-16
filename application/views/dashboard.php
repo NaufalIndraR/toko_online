3895382ee195af7b5c4a3d241c42321e9897139b
NaufalIndraR <naufal.indrarizky@gmail.com>
2022-04-16 11:10:49 +0700

    16 April 2022

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -11,21 +11,22 @@
     </div>
     </div>
 
-    <div class="row text-center mt-3">
+    <div class="row text-center mt-4">
 
     <?php foreach ($barang as $brg) : ?>
+<div class="card mt-3 ml-3" style="width: 18rem;">
+  <img src="<?php echo base_url().'/uploads/'.$brg->gambar ?>" class="card-img-top " alt="...">
+  <div class="card-body">
+    <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
+    <small><?php echo $brg->keterangan ?></small><br>
+    <span class="badge rounded-pill bg-success text-light mb-2">Rp. <?php echo number_format($brg->harga, 0, ',', '.')  ?></span>
+    <?php echo anchor('dashboard/tambahkan_ke_keranjang/' .$brg
+    ->id_brg, '<div class="btn btn-sm btn-primary"> Tambahkan ke keranjang </div>') ?>
+   <?php echo anchor('dashboard/detail/' .$brg
+    ->id_brg, '<div class="btn btn-sm btn-success">Detail</div>') ?>
+  </div>
+</div>
 
-        <div class="card ml-3" style="width: 16rem;">
-          <img src="<?php echo base_url().'/uploads/'.$brg->gambar ?>" class="card-img-top " alt="...">
-          <div class="card-body">
-            <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
-            <small><?php echo $brg->keterangan ?></small><br>
-            <span class="badge rounded-pill bg-success mb-3">Rp. <?php echo $brg->harga ?></span>
-            <a href="#" class="btn btn-primary">Tambah ke keranjang</a>
-            <a href="#" class="btn btn-success">Detail</a>
-          </div>
-        </div>
-
-        <?php endforeach; ?>
+<?php endforeach; ?>
     </div>
 </div>
